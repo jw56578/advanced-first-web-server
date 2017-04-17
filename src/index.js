798fcdff369fa4a11afce05262b4140fafb261d9
Jon Woo <jw56578@gmail.com>
2017-04-16 20:59:51 -0500

    turn on express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3002;
+
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log("Error!", err);
+  }
+
+  return console.log("Listening on: http://localhost:" + PORT);
+});
 
